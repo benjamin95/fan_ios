40f502a2400c41a2c702469b77aee6142e44d80a
Benjamin <benjamin.truillet@gmail.com>
2023-12-26 20:43:34 +0100

    Mise en francais

--- fanv3/fanv3App.swift
+++ fanv3/fanv3App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct fanv3App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.locale, .init(identifier: "fr_FR")) 
         }
     }
 }
