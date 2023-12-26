77997fb9b2ac079a6d3c1040798fad452fcbe2df
Benjamin <benjamin.truillet@gmail.com>
2023-12-26 18:20:37 +0100

    Initial Commit

---
+++ fanv3/fanv3App.swift
@@ -0,0 +1,17 @@
+//
+//  fanv3App.swift
+//  fanv3
+//
+//  Created by Benjamin Truillet on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct fanv3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
